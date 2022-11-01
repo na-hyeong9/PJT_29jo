6f28b4ceb06b39ef6a90741be3c29ec0e19bdf3d
jincde <asdzxc@kakao.com>
2022-11-01 17:46:03 +0900

    feat : accounts/detail

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,10 +4,10 @@ from . import views
 app_name = "accounts"
 
 urlpatterns = [
-    # path("<str:username>/", views.detail, name="detail"),
     path("signup/", views.signup, name="signup"),
     path("signin/", views.signin, name="signin"),
     path("signout/", views.signout, name="signout"),
+    path("<int:user_pk>/follow/", views.follow, name="follow"),
+    path("<str:username>/", views.detail, name="detail"),
     # path("update/", views.update, name="update"),
-    # path("<int:user_id>/follow/", views.follow, name="follow"),
 ]
