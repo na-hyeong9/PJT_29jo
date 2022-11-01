ce05e6a75edf228618790f88e662b46d8146ab7f
jincde <asdzxc@kakao.com>
2022-11-01 15:14:51 +0900

    feat : accounts/ signin out

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,8 +6,8 @@ app_name = "accounts"
 urlpatterns = [
     # path("<str:username>/", views.detail, name="detail"),
     path("signup/", views.signup, name="signup"),
-    # path("signin/", views.signin, name="signin"),
-    # path("signout/", views.signout, name="signout"),
+    path("signin/", views.signin, name="signin"),
+    path("signout/", views.signout, name="signout"),
     # path("update/", views.update, name="update"),
     # path("<int:user_id>/follow/", views.follow, name="follow"),
 ]
