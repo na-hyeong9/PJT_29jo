ad6cf6a4e2c89d1ce4c5ff38e815e21eba3b41da
avocad06 <miggo2704@gmail.com>
2022-11-01 18:07:12 +0900

    feat: products/detail_reviews

--- products/views.py
+++ products/views.py
@@ -1,5 +1,25 @@
+import statistics
 from django.shortcuts import render
+from .models import Product
+from .models import Review
 
 # Create your views here.
+
 def detail(request, product_pk):
-    return render(request, "products/detail.html")
+    products = Product.objects.all()
+    reviews = Review.objects.all()
+    values_ = reviews.values('grade_1', 'grade_2', 'grade_3', 'grade_4')
+
+        # annotate
+        
+        # for value in values:
+        #     avg_score = statistics.mean(list(value.values()))
+        #     break
+        # review.grade_1 = avg_score
+        # review.save()
+  
+    context = {
+        "products" : products,
+        "reviews" : reviews,
+    }
+    return render(request, "products/detail.html", context)
